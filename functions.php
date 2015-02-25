6b05a30ec50d09e9d154bd9fca4527821944d504
Tim Spinks <tim@ninthlink.com>
2015-02-25 11:13:53 -0800

    added visual composer as embedded part of progo

--- functions.php
+++ functions.php
@@ -356,7 +356,7 @@ require get_template_directory() . '/includes/bootstrap-wp-navwalker-collapse.ph
 /**
  * TGM pluging activation for visual composer
  */
-// require get_template_directory() . '/plugin/plugin-activation.php';
+require get_template_directory() . '/plugin/plugin-activation.php';
 
 
 /**
@@ -388,3 +388,7 @@ function load_custom_wp_admin_style() {
         wp_enqueue_style( 'custom_wp_admin_css' );
 }
 add_action( 'admin_enqueue_scripts', 'load_custom_wp_admin_style' );
+
+
+
+?>
