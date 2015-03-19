e19e5a027b184495064fa4ab4792ae70db9c917f
Tim Spinks <tim@ninthlink.com>
2015-03-19 12:23:53 -0700

    so many more nested bootstrap rows and cols...

--- search.php
+++ search.php
@@ -11,22 +11,40 @@ get_header(); ?>
 
 		<?php if ( have_posts() ) : ?>
 
-			<header class="page-header">
-				<h2 class="page-title"><?php printf( __( 'Search Results for: %s', 'pgb' ), '<span>' . get_search_query() . '</span>' ); ?></h2>
-			</header><!-- .page-header -->
+			<div class="row">
+
+				<div class="col-md-12">
+
+					<header class="page-header">
+
+						<h2 class="page-title"><?php printf( __( 'Search Results for: %s', 'pgb' ), '<span>' . get_search_query() . '</span>' ); ?></h2>
+
+					</header><!-- .page-header -->
+
+				</div>
+
+			</div>
 
 			<?php // start the loop. ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php tha_entry_before(); ?>
 
-				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<article id="post-<?php the_ID(); ?>" <?php post_class( 'row' ); ?>>
 
 					<?php tha_entry_top(); ?>
 
 					<?php get_template_part( 'posts', 'header' ); ?>
 
-					<?php get_template_part( 'content', get_post_format() ); ?>
+					<div class="col-md-12">
+
+						<div class="row">
+
+							<?php get_template_part( 'content', get_post_format() ); ?>
+
+						</div>
+
+					</div>
 
 					<?php get_template_part( 'posts', 'footer' ); ?>
 
