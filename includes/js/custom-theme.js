13803e7d3e41b8d6f8be7abb20ceb2dcdfa245a9
Tim Spinks <tim@ninthlink.com>
2015-06-16 10:13:04 -0700

    js nav icon fix

--- includes/js/custom-theme.js
+++ includes/js/custom-theme.js
@@ -1,9 +1,9 @@
-(function () {
+(function ($) {
 	
 	// If Search Field empty, focus on submit
-	jQuery(document).on('click', 'form[role="search"] button[type="submit"]', function(event) {
+	$(document).on('click', 'form[role="search"] button[type="submit"]', function(event) {
 		event.preventDefault();
-		var searchform = jQuery(this).closest('form'),
+		var searchform = $(this).closest('form'),
 			searchinput = searchform.find('input');
 		if ( ! searchinput.val() ) {
 			searchinput.focus();
@@ -13,18 +13,18 @@
 		};
 	});
 
-	jQuery(document).ready(function(){
+	$(window).load(function(){
 
 		// Vertically center nav-bar logo image (mobile logo)
-		if ( jQuery('.navbar-brand img').is(':visible') ) {
-			var brandimgh = jQuery('.navbar-brand img').height(),
-				mainnavh = jQuery('#main-nav').height(),
-				offset = ( mainnavh - brandimgh ) / 2;
-			jQuery('.navbar-brand').css({
+		if ( $('.navbar-brand img').is(':visible') ) {
+			var brandimgh = $('.navbar-brand img').height(),
+				mainnavh = $('#main-nav').height(),
+				offset = Math.min( ( mainnavh - brandimgh ) / 2, 10 );
+			$('.navbar-brand').css({
 				"padding-top": offset + "px",
 				"padding-bottom": offset + "px"
 			}); 
 		}
 
 	});
-})(jQuery);
+}(jQuery));
